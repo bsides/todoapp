858795c6b4541dd0b7ebdfed24e81cd8f4fa7b79
Rafael Pereira <bsides@RafaMBP.local>
2018-04-11 16:27:18 -0500

    Editing is now possible but should change to a controlled component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,14 +6,31 @@ export default class Todo extends Component {
     return (
       <ul>
         {todos.length > 0 &&
-          todos.map((todo, i) => {
+          todos.map(todo => {
             return (
-              <li key={i}>
-                {todo}{' '}
+              <li
+                key={todo.id}
+                data-todo={todo.id}
+                onClick={this.props.toggleTodo}
+                onContextMenu={this.props.editTodo}
+              >
+                <span
+                  style={{
+                    textDecoration: todo.isDone ? 'line-through' : 'none',
+                    display: todo.isEditing ? 'none' : 'inline'
+                  }}
+                >
+                  {todo.text}
+                </span>
+                <input
+                  type={todo.isEditing ? 'text' : 'hidden'}
+                  value={todo.text}
+                  onChange={this.props.handleChange}
+                />
                 <button
                   type="button"
                   className="delete-todo"
-                  data-todo={todo}
+                  data-todo={todo.id}
                   onClick={this.props.removeTodo}
                 >
                   x
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Todo from './Todo'
 
 export default class Todos extends Component {
+  initialTodoId = 0
   state = {
     inputTodo: '',
     todos: []
@@ -11,18 +12,45 @@ export default class Todos extends Component {
   }
   handleSubmit = e => {
     e.preventDefault()
-    const todo = this.state.inputTodo
+    const text = this.state.inputTodo
+    const todo = {
+      id: ++this.initialTodoId,
+      text,
+      isDone: false,
+      isEditing: false
+    }
     this.setState(prevState => ({
       todos: [...prevState.todos, todo],
       inputTodo: ''
     }))
   }
   removeTodo = e => {
-    const todoToBeRemoved = e.target.dataset.todo
-    const todoRemoved = this.state.todos.filter(
-      item => item !== todoToBeRemoved
+    const todoToBeRemoved = parseInt(e.currentTarget.dataset.todo, 10)
+    const todosFiltered = this.state.todos.filter(
+      item => item.id !== todoToBeRemoved
     )
-    this.setState({ todos: todoRemoved })
+    this.setState({ todos: todosFiltered })
+  }
+  editTodo = e => {
+    e.preventDefault()
+    const todoToBeEdited = parseInt(e.currentTarget.dataset.todo, 10)
+    const todosFiltered = this.state.todos.map(item => {
+      if (item.id === todoToBeEdited) {
+        item.isEditing = !item.isEditing
+      }
+      return item
+    })
+    this.setState({ todos: todosFiltered })
+  }
+  toggleTodo = e => {
+    const todoToBeToggled = parseInt(e.currentTarget.dataset.todo, 10)
+    const todosFiltered = this.state.todos.map(item => {
+      if (item.id === todoToBeToggled) {
+        item.isDone = !item.isDone
+      }
+      return item
+    })
+    this.setState({ todos: todosFiltered })
   }
   render() {
     return (
@@ -35,7 +63,12 @@ export default class Todos extends Component {
             onChange={this.handleChange}
           />
           <input type="submit" value="Add Todo" onClick={this.handleSubmit} />
-          <Todo todos={this.state.todos} removeTodo={this.removeTodo} />
+          <Todo
+            removeTodo={this.removeTodo}
+            editTodo={this.editTodo}
+            toggleTodo={this.toggleTodo}
+            {...this.state}
+          />
         </form>
       </div>
     )
