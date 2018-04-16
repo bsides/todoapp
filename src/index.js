9f44d771c86dd588fe677082baabe26da2c5e070
Rafael Pereira <rafaelbsides@gmail.com>
2018-04-15 21:48:35 -0500

    Reduxing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
---
+++ src/redux/reducers.js
@@ -0,0 +1,21 @@
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          isDone: false
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(
+        todo =>
+          todo.id === action.id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    default:
+      return state
+  }
+}
+export default todos
