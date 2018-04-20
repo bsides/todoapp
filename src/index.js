c3a445fcd4c8ff60f7756ddf8f1acbb4f829b3d3
Rafael Pereira <rafaelbsides@gmail.com>
2018-04-20 14:55:42 -0500

    Added SASS to the toolchain

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './assets/styles/index.css'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './redux/reducers'
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
 export const addTodo = text => ({ type: 'ADD_TODO', text })
 export const deleteTodo = id => ({ type: 'DELETE_TODO', id })
 export const editTodo = (id, text) => ({ type: 'EDIT_TODO', id, text })
-export const toggleTodo = id => ({ type: 'COMPLETE_TODO', id })
+export const toggleTodo = id => ({ type: 'TOGGLE_TODO', id })
